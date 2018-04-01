fad7e9e4029758af4c6d701ef11192a2a8b791fc
Valeria Protsan <valeriyapro@gmail.com>
2018-04-01 17:54:22 -0400

    added index to email column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180401213650) do
+ActiveRecord::Schema.define(version: 20180401215054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20180401213650) do
     t.datetime "updated_at", null: false
     t.bigint "article_id"
     t.index ["article_id"], name: "index_users_on_article_id"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "comments", "articles"
