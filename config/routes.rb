1df6aeef6484a4b5171fb9985f06a7572fac4015
Valeria Protsan <valeriyapro@gmail.com>
2018-04-01 17:13:48 -0400

    added user controller with index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get 'users/new'
+
+  # get 'welcome/index'
   resources :articles do
     resources :comments
   end
   root 'welcome#index'
+
+  get  '/signup',  to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
