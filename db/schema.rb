680239f80779bc4fd4fc6f76fadae8de08406cfd
Valeria Protsan <valeriyapro@gmail.com>
2018-04-02 15:50:20 -0400

    persistent cookies are done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180401220728) do
+ActiveRecord::Schema.define(version: 20180402180204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20180401220728) do
     t.datetime "updated_at", null: false
     t.bigint "article_id"
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["article_id"], name: "index_users_on_article_id"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
