658609ca0e45f45422279cb1c5e5e47ef4e5eb62
Valeria Protsan <valeriyapro@gmail.com>
2018-04-02 18:35:46 -0400

    adding before action to update and edit methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:update, :edit]
+
   def new
   end
 
@@ -43,6 +45,13 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in"
+        redirect_to login_url
+      end
     end
 
 end
