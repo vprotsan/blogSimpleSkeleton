892f7077d4ca2d461cb2260ed6d72c74f329894f
Valeria Protsan <valeriyapro@gmail.com>
2018-04-02 16:33:56 -0400

    forgetting users when log out

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,13 +17,17 @@ class User < ApplicationRecord
   end
 
   #returns a random token
-  def User.new_token
+  def self.new_token
     SecureRandom.urlsafe_base64
   end
 
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 
   def authenticated?(remember_token)
