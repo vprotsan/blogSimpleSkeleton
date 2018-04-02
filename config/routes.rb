de12bd8c52850be9a3bc2da65ee3379e939cb097
Valeria Protsan <valeriyapro@gmail.com>
2018-04-02 11:06:20 -0400

    user login in progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'session/new'
+
   get 'users/new'
 
   # get 'welcome/index'
@@ -10,5 +12,9 @@ Rails.application.routes.draw do
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
   resources :users
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
